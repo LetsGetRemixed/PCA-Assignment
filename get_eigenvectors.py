cc428ffd25ad0763318e97682cb6daa97aa58757
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2023-10-19 18:32:25 -0500

    Update get_eigenvectors.py

--- get_eigenvectors.py
+++ get_eigenvectors.py
@@ -2,17 +2,33 @@ import numpy as np
 import cv2
 
 def get_eigenvectors(data_file_path, digit):
-    """
-    Computes the mean vector and eigenvectors of the covariance matrix of a given digit in a dataset.
-
-    Args:
-        data_file_path (str): The path to the dataset file.
-        digit (int): The digit to extract from the dataset.
-
-    Returns:
-        tuple: A tuple containing the mean vector and eigenvectors of the covariance matrix.
-    """
     
     # YOUR CODE HERE
+    digit_pixels = []
+
+    # readin dat data
+    with open(data_file_path, 'r') as file:
+        for line in file:
+            row = line.strip().split(',')
+            label = int(row[0])
+            if label == digit:
+                pixel_values = np.array(list(map(int, row[1:])))
+                digit_pixels.append(pixel_values)
+
+    # list of pixels to array
+    digit_pixels = np.array(digit_pixels)
+
+    # mean vector
+    mean_vector = np.mean(digit_pixels, axis=0)
+
+    # covariance matrix
+    cov_matrix = np.cov(digit_pixels, rowvar=False)
+
+    # eigen values
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
+
+    # sort them values cuzzo
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvectors = eigenvectors[:, sorted_indices]
 
     return mean_vector, eigenvectors
